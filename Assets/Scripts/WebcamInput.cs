b184fe447baa5311b47c32809fa5a297ac94d53a
rob verhoef <16617@ma-web.nl>
2015-05-27 08:45:17 +0200

    Added webcam streaming, added Oculus Rift support

---
+++ Assets/Scripts/WebcamInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamInput : MonoBehaviour 
+{	
+	private float _screenHeight;
+	private float _screenWidth;
+
+	void Start () 
+	{
+		//scales the plane to the size of the screen.
+		_screenHeight = Camera.main.orthographicSize / 5;
+		_screenWidth = _screenHeight / Screen.height * Screen.width;
+		transform.localScale = new Vector3 (_screenWidth,1,_screenHeight);
+		 
+		//displays the feed from the webcam upon the plane.
+		WebCamTexture _webcam = new WebCamTexture ();
+		Renderer renderer = GetComponent<Renderer> ();
+		renderer.material.mainTexture = _webcam;
+		_webcam.Play ();
+	}
+
+	void Update () 
+	{
+	
+	}
+}
