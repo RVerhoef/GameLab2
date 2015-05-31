dc5b44893b51bc6b3ffa5aa166d7c74af1aad4ea
rob verhoef <16617@ma-web.nl>
2015-05-31 17:39:51 +0200

    Added intro movie, added HUD art files

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour 
+{
+	private float _screenHeight;
+	private float _screenWidth;
+	[SerializeField] private MovieTexture _movieTexture;
+
+	void Start () 
+	{
+		//scales the plane to the size of the screen.
+		_screenHeight = Camera.main.orthographicSize / 5;
+		_screenWidth = _screenHeight / Screen.height * Screen.width;
+		transform.localScale = new Vector3 (_screenWidth,1,_screenHeight);
+
+		//play the intro movie on the plane.
+		GetComponent<Renderer>().material.mainTexture = _movieTexture;
+		_movieTexture.Play();
+	}
+
+	void Update ()
+	{
+		if (_movieTexture.isPlaying == false) 
+		{
+			Destroy(gameObject);
+		}
+	}	
+}
+ 
